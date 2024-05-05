325805eace561b8b596ac8a095dde9d135f290d1
Oleg <logosemi@mail.ru>
2024-05-05 20:54:38 +0500

    new kod

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         int balans = 111;
-        int price = 1100;
+        int price = 999;
         int replenishment = (balans + price);
-        {
-            if (replenishment > 1000) {
-                int bonus = price  / 100;
-                System.out.println(replenishment + " рублей теберь на балансе " );
-                System.out.println("Плюс " + bonus + " бонусов на счёт ");
+
+        if (price > 1000) {
+            int bonus = price / 100 * 1;
+            System.out.println(replenishment + " рублей теберь на балансе ");
+            System.out.println("Плюс " + bonus + " бонусных рублей ");
 
 
-
-            }  else {
-                System.out.println(replenishment + " на балансе");
-            }
+        } else {
+            System.out.println(replenishment + " на балансе");
         }
+
 
     }
 }
